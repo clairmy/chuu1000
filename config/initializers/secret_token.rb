ac63dcb1df561cdcae4fc2063dee79277a1b9a39
nbp <nbp@nbpui-MacBook-Air.local>
2013-11-01 22:47:24 +0900

    * Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chuu1000::Application.config.secret_key_base = '7e2399cb40610857bc7a4c87750f2969526fd40ac0b7a313f1383e39157ded38855a7f41a4a305e710945e5396ff322e18f22a5bde4e43d4dd0d4db01d8c0e2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chuu1000::Application.config.session_store :cookie_store, key: '_chuu1000_session'
